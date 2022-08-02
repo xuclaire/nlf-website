72bee32659c52fa2af092a25c894e9a14d104949
Isabel Tu <iht1@rice.edu>
2022-08-01 23:39:59 -0700

    added photo prop

--- src/shared-components/leafImageContainer.jsx
+++ src/shared-components/leafImageContainer.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import LeafImage from '../leaf.svg';
+import PropTypes from 'prop-types';
 import LeafShadowImage from '../leafShadow.svg';
 
-function LeafImageContainer() {
+function LeafImageContainer(props) {
+  const { photo } = props;
   return (
     <div className="leaf-outline">
       <img className="leaf-shadow" src={LeafShadowImage} alt="leaf-shadow" />
-      <img className="leaf" src={LeafImage} alt="leaf" />
+      <img className="leaf" src={photo} alt="leaf" />
     </div>
   );
 }
+
+LeafImageContainer.propTypes = {
+  photo: PropTypes.oneOf(['img', 'svg']).isRequired,
+};
 
 export default LeafImageContainer;
