eb09be4652b599b5c24b357f2923376911866b5c
Christine Lee <christinelee@Christines-MacBook-Air.local>
2022-08-09 23:25:35 -0600

    updated dependencies and made eslint softer to make coding exp better

--- src/index.js
+++ src/index.js
@@ -1,37 +1,36 @@
-/* eslint-disable react/jsx-filename-extension */
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import reportWebVitals from "./reportWebVitals";
-import Home from "./pages/home";
-import AboutUs from "./pages/about_us";
-import HouseChurch from "./pages/house_church";
-import Events from "./pages/events";
-import Classes from "./pages/classes";
-import FAQ from "./pages/faq";
-import GetConnected from "./pages/get_connected";
-import Navbar from "./pages/navbar";
-import Footer from "./pages/footer/footer";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
+import Home from './pages/home';
+import AboutUs from './pages/about_us';
+import HouseChurch from './pages/house_church';
+import Events from './pages/events';
+import Classes from './pages/classes';
+import FAQ from './pages/faq';
+import GetConnected from './pages/get_connected';
+import Navbar from './pages/navbar';
+import Footer from './pages/footer/footer';
 
 export default function App() {
-  return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/house_church" element={<HouseChurch />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/classes" element={<Classes />} />
-          <Route path="/faq" element={<FAQ />} />
-          <Route path="/get_connected" element={<GetConnected />} />
-        </Routes>
-      </Router>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<AboutUs />} />
+                    <Route path="/house_church" element={<HouseChurch />} />
+                    <Route path="/events" element={<Events />} />
+                    <Route path="/classes" element={<Classes />} />
+                    <Route path="/faq" element={<FAQ />} />
+                    <Route path="/get_connected" element={<GetConnected />} />
+                </Routes>
+            </Router>
+            <Footer />
+        </>
+    );
 }
 ReactDOM.render(<App />, document.getElementById("root"));
 // If you want to start measuring performance in your app, pass a function
