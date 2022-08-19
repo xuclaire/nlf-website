0ea8f42c87fd898bcd89e4afddb96c3961803af3
Claire Xu <claire02.xu@gmail.com>
2022-08-18 20:51:13 -0500

    what we do start

---
+++ src/pages/what_we_do/what_we_do.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './what_we_do_styles.css';
+
+function WhatWeDo() {
+  return (
+    <>
+      <div className="section1">
+        <h1>What is New Life?</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor <br></br>incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis <br></br>nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <button>Learn About House Church</button>
+      </div>
+      <div className="section2">
+        <h1>We make disciples and model life with Christ through house church.</h1>
+      </div>
+      <div className="section3">
+        <h1>New Life Fellowship Church</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor <br></br>incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis <br></br>nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <button>New Life Fellowship Church</button>
+        <button>Sign up for a Ride to Church!</button>
+      </div>
+    </>
+  );
+}
+      
+export default WhatWeDo;
+      
