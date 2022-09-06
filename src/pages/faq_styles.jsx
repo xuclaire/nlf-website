fa7dd793b70d4b4940f41ade313605610947e021
fdandaya <94016032+fdandaya@users.noreply.github.com>
2022-09-06 03:56:53 -0500

    Update faq_styles.jsx

--- src/pages/faq_styles.jsx
+++ src/pages/faq_styles.jsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const FaqPage = styled.div`
     background: #F8F3E3;
-    display: ;
 `;
 export const Pagehead = styled.div`
     font-size: 64px;
@@ -12,10 +11,22 @@ export const Pagehead = styled.div`
     margin-left: 60px;
     margin-right: 60px;
     margin-bottom: 10px;
-
-    
+`;
+export const General = styled.div`
+    background: #F8F3E3;
+    padding-bottom: 50px;
+`;
+export const HouseChurch = styled.div`
+    background: #3A4520;
+    padding-bottom: 50px;
+`;
+export const NewLife = styled.div`
+    background: #F8F3E3;
+    padding-bottom: 50px;
 `;
 export const Accordion = styled.div`
+    margin-left: 60px;
+    margin-right: 60px;
 `;
 export const Span = styled.span`
     display: inline-block;
@@ -25,7 +36,8 @@ export const CategoryHeaders = styled.div`
     font-size: 60px;
     color: #3A4520;
     font-weight: 700;
-    margin-bottom: 30px;
+    margin-left: 60px;
+    margin-right: 60px;
 `;
 export const HcCategoryHeader = styled.div`
     font-family: palatino;
@@ -34,60 +46,36 @@ export const HcCategoryHeader = styled.div`
     font-weight: 700;
     margin-left: 60px;
     margin-right: 60px;
-    margin-bottom: 30px;
 `;
-export const General = styled.div`
-    background: #F8F3E3;
-    margin-bottom: 52px;
-    margin-left: 60px;
-    margin-right: 60px;
-`;
-export const HouseChurch = styled.div`
-    background: #3A4520;
-    color: white ;
-`;
-export const NewLife = styled.div`
-    background: #F8F3E3;
-    display:  ;
+
+export const Title = styled.div`
+    display: flex;
+    font-family: palatino;
     color: #3A4520;
-    margin-left: 60px;
-    margin-right: 60px;
-    margin-bottom: 0px;
+    font-size: 40px;
+    justify-content: space-between;
+    align-items: center;
+    cursor: pointer;
+    font-weight: 500;
+`;
+export const HcTitle = styled.div`
+    display: flex;
+    font-family: palatino;
+    color: white;
+    font-size: 40px;
+    justify-content: space-between;
+    align-items: center;
+    cursor: pointer;
 `;
 export const Item = styled.div`
     font-family: palatino;
     color: #3A4520;
     border-style: none none solid;
-    margin-bottom: 0px;
-    padding: 10px 20px;
+    font-size: 30px;
 `;
 export const HcItem = styled.div`
     font-family: palatino;
     color: white;
     border-style: none none solid;
-    margin-left: 60px;
-    margin-right: 60px;
-    padding: 10px 20px;
+    font-size: 30px;
 `;
-export const Title = styled.div`
-    
-    display: flex;
-    font-family: palatino;
-    color: #3A4520;
-    justify-content: space-between;
-    align-items: center;
-    background:;
-    cursor: pointer;
-    
-`;
-export const HcTitle = styled.div`
-    
-    display: flex;
-    font-family: palatino;
-    color: white;
-    justify-content: space-between;
-    align-items: center;
-    background:;
-    cursor: pointer;
-`;
-
