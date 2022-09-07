08ee61700e77ff2568a7bd0ee114180af52d66e6
Christine Lee <christinelee@Christines-Air.attlocal.net>
2022-09-07 01:50:40 -0500

    pastor modal css needs work

--- src/pages/leadership/leadership.jsx
+++ src/pages/leadership/leadership.jsx
@@ -25,6 +25,13 @@ import PeterPic from '../../images/peter_chung.jpeg';
 import SusanPic from '../../images/susan.jpeg';
 
 function Leadership() {
+  const [showPE, setShowPE] = useState(true);
+  function handleToggle() {
+    return setShowPE((prevState) => {
+      return !prevState;
+    });
+  }
+
   return (
     <>
       <div className="title-section">
@@ -37,9 +44,21 @@ function Leadership() {
       <div className="meet-the-pastors-section">
         <h2 className="meet-the-pastors-title">Meet the Pastors</h2>
         <div className="pastor-bios">
-          <PastorCard photo={PastorEricPic} name={'Pastor Eric Shin'} role={'Senior Pastor'} />
-          <PastorCard photo={PastorSangPic} name={'Pastor Sang'} role={'College Pastor'} />
-          <PastorCard photo={PastorJustyPic} name={'Pastor Justy'} role={'College Pastor'} />
+          <div className="pastor-box" onClick={handleToggle}>
+            <PastorCard photo={PastorEricPic} name={'Pastor Eric Shin'} role={'Senior Pastor'} />
+            {showPE &&
+              <PastorModal
+                
+                name="Pastor Eric Shin"
+                role="Senior Pastor"
+                bio="As the Senior Pastor at New Life, Eric Shin has founded the church together with about twenty members on August 10, 1997. He grew up in New York City and was saved at the age of eighteen at a youth conference. After experiencing joys and challenges of following Christ and serving in His local church during high school and college years, he left behind aspirations of being a lawyer in his hometown and followed God’s call to seminary. Pastor Eric (affectionately known as PE) pastored a church in Boston for several years before coming to New Life with his lovely wife Lynette and their three children, Enoch, Caleb, and Josiah."/>}
+          </div>
+          <div className="pastor-box">
+            <PastorCard photo={PastorSangPic} name={'Pastor Sang'} role={'College Pastor'} />
+          </div>
+          <div className="pastor-box">
+            <PastorCard photo={PastorJustyPic} name={'Pastor Justy'} role={'College Pastor'} />
+          </div>
         </div>
         <div className="pastor-modals"></div>
       </div>
@@ -63,20 +82,20 @@ function Leadership() {
         </div>
         <div className="shepherd-profiles-center">
           <div className="shepherd-profiles">
-            <ShepherdProfile photo={AaronPic} name={'Aaron Lin'} house_church={'Mongolia'} link="/mongolia"/>
-            <ShepherdProfile photo={ChristinePic} name={'Christine Lee'} house_church={'Manado'} link="/manado"/>
-            <ShepherdProfile photo={CiciPic} name={'Cici Du'} house_church={'Anatolia'} link="/anatolia"/>
-            <ShepherdProfile photo={ClairePic} name={'Claire Xu'} house_church={'Laos'} link="/laos"/>
-            <ShepherdProfile photo={DamianPic} name={'Damian Andaya'} house_church={'North Africa'} link="/north_africa"/>
-            <ShepherdProfile photo={EnochPic} name={'Enoch Chen'} house_church={'Central Japan'} link="/central_japan"/>
-            <ShepherdProfile photo={EricPic} name={'Eric Ou'} house_church={'U of H'} link="/uofh"/>
-            <ShepherdProfile photo={EstherPic} name={'Esther Lee'} house_church={'Phnom Penh'} link="/phnom_penh"/>
-            <ShepherdProfile photo={GeorgePic} name={'George Hung'} house_church={'Phnom Penh'} link="/phnom_penh"/>
+            <ShepherdProfile photo={AaronPic} name={'Aaron Lin'} house_church={'Mongolia'} link="/mongolia" />
+            <ShepherdProfile photo={ChristinePic} name={'Christine Lee'} house_church={'Manado'} link="/manado" />
+            <ShepherdProfile photo={CiciPic} name={'Cici Du'} house_church={'Anatolia'} link="/anatolia" />
+            <ShepherdProfile photo={ClairePic} name={'Claire Xu'} house_church={'Laos'} link="/laos" />
+            <ShepherdProfile photo={DamianPic} name={'Damian Andaya'} house_church={'North Africa'} link="/north_africa" />
+            <ShepherdProfile photo={EnochPic} name={'Enoch Chen'} house_church={'Central Japan'} link="/central_japan" />
+            <ShepherdProfile photo={EricPic} name={'Eric Ou'} house_church={'U of H'} link="/uofh" />
+            <ShepherdProfile photo={EstherPic} name={'Esther Lee'} house_church={'Phnom Penh'} link="/phnom_penh" />
+            <ShepherdProfile photo={GeorgePic} name={'George Hung'} house_church={'Phnom Penh'} link="/phnom_penh" />
             <ShepherdProfile photo={JoshPic} name={'Josh Ni'} house_church={'Siem Reap'} link="/siem_reap" />
             <ShepherdProfile photo={KristinePic} name={'Kristine Yuan'} house_church={'Siem Reap'} link="/siem_reap" />
-            <ShepherdProfile photo={PeterPic} name={'Peter Chung'} house_church={'Tunisia'} link="/tunisia"/>
-            <ShepherdProfile photo={RebekahPic} name={'Rebekah Heo'} house_church={'CMU'} link="/cmu"/>
-            <ShepherdProfile photo={SusanPic} name={'Susan Do'} house_church={'U of H'} link="/uofh"/>
+            <ShepherdProfile photo={PeterPic} name={'Peter Chung'} house_church={'Tunisia'} link="/tunisia" />
+            <ShepherdProfile photo={RebekahPic} name={'Rebekah Heo'} house_church={'CMU'} link="/cmu" />
+            <ShepherdProfile photo={SusanPic} name={'Susan Do'} house_church={'U of H'} link="/uofh" />
           </div>
         </div>
       </div>
